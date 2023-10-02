fb38207e365aca4b2e1872f86bbff135a104477c
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-10-02 11:42:09 -0400

    Add transition animations on scroll.

--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -2,3 +2,16 @@
     .addEventListener('click', () => {
         document.getElementById('nav-menu').classList.toggle('hidden');
     });
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(e => {
+        if (e.isIntersecting) {
+            e.target.classList.add('show');
+        } else {
+            e.target.classList.remove('show');
+        }
+    });
+})
+
+const hiddenElements = document.querySelectorAll('.hide');
+hiddenElements.forEach(el => observer.observe(el));
