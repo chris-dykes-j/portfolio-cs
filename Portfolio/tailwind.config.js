fbec7ec21ce559ef05cb116cc4568b804fdd14ef
Christopher Dykes <chris.t.c.dykes@gmail.com>
2023-10-02 12:32:20 -0400

    Add dark mode functionality. Refactor style sheets.

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -1,12 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Views/**/*.cshtml"], 
+  content: ["./Views/**/*.cshtml"],
   theme: {
     extend: {
       colors: {
-        mint: "#B9E9E8"
+        mint: "#B9E9E8",
+        nord0: "#2e3440",
+        nord1: "#3b4252",
+        nord2: "#434c5e",
+        nord3: "#4c566a",
+        nord4: "#d8dee9",
+        nord5: "#e5e9f0",
+        nord6: "#eceff4",
+        nord7: "#8fbcbb",
+        nord8: "#88c0d0",
+        nord9: "#81a1c1",
+        nord10: "#5e81ac",
+        nord11: "#bf616a",
+        nord12: "#d08770",
+        nord13: "#ebcb8b",
+        nord14: "#a3be8c",
+        nord15: "#b48ead",
+        'nord-dark': "#292E39",
       },
     },
   },
+  darkMode: 'class',
   plugins: [],
 }
--- Portfolio/wwwroot/js/site.js
+++ Portfolio/wwwroot/js/site.js
@@ -1,8 +1,10 @@
-﻿document.getElementById('nav-button')
+﻿// Navigation Menu
+document.getElementById('nav-button')
     .addEventListener('click', () => {
         document.getElementById('nav-menu').classList.toggle('hidden');
     });
 
+// Animation
 const observer = new IntersectionObserver(entries => {
     entries.forEach(e => {
         if (e.isIntersecting) {
@@ -15,3 +17,22 @@ const observer = new IntersectionObserver(entries => {
 
 const hiddenElements = document.querySelectorAll('.hide');
 hiddenElements.forEach(el => observer.observe(el));
+
+// Dark Mode Toggle
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
+} else {
+    document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light'
+}
+
+function toggleDarkMode() {
+    if (localStorage.theme === 'dark') {
+        document.documentElement.classList.remove('dark');
+        localStorage.theme = 'light';
+    } else {
+        document.documentElement.classList.add('dark')
+        localStorage.theme = 'dark';
+    }
+}
